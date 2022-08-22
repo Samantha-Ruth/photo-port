0b33b42598ba60b23ef52e05c057023d8e1f9b0c
Samantha-Ruth <sam.haberman@gmail.com>
2022-08-22 11:34:58 -0600

    created About and Nav sections

---
+++ src/components/Pet.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Pet = () => {
+    return(
+        // React.createElement("div", {}, [
+        // React.createElement("h1", {}, "Ginger"),
+        // React.createElement("p", {}, "breed: Brittany Spaniel")
+        // ])
+        <div>
+            <h1>Ginger</h1>
+            <p>breed: Brittany Spaniel</p>
+        </div>
+    )
+}
+
+export default Pet;
