d8d8167f77aa26bb17850854243a36ad07281d23
Samantha-Ruth <sam.haberman@gmail.com>
2022-08-29 18:58:39 -0600

    added test to Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Modal({currentPhoto}) {
+const Modal = ({ onClose, currentPhoto }) => {
     const {name, category, description, index} = currentPhoto;
 
   return (
@@ -9,7 +9,7 @@ function Modal({currentPhoto}) {
         <h3 className="modalTitle">{name}</h3>
         <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
         <p>{description}</p>
-        <button type="button">Close this modal</button>
+        <button onClick={onClose} type="button">Close this modal</button>
       </div>
     </div>
   );
