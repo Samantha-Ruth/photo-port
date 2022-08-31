6a6019986acca49c1d815a408adc0a76ee9283e5
Samantha-Ruth <sam.haberman@gmail.com>
2022-08-30 20:55:06 -0600

    fixed photo rendering issue by removing .default from img line - potentially a version problem

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -136,7 +136,7 @@ const PhotoList = ({ category }) => {
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`).default}
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
             onClick={() => toggleModal(image, i)}
